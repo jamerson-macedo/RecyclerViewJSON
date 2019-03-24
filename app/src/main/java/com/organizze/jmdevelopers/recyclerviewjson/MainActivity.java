fe5ea9a145df7418e856b90a7005dc4ac6bbf672
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2019-03-24 19:41:19 -0300

    recebendo json ok

--- app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/MainActivity.java
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/MainActivity.java
@@ -1,10 +1,23 @@
 package com.organizze.jmdevelopers.recyclerviewjson;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -19,6 +32,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        itemArrayList=new ArrayList<>();
+        Context context;
+        requestQueue=Volley.newRequestQueue(this);
+        parseJSON();
+    }
 
+    private void parseJSON() {
+        String URL="https://pixabay.com/api/?key=11982729-98e22b893c204e6f42460c30b&q=yellow+flowers&image_type=photo";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray=response.getJSONArray( "hits");
+                    for (int i =0;i<jsonArray.length(); i++){
+                        JSONObject jsonHITS=jsonArray.getJSONObject(i);
+                        String criador=jsonHITS.getString("user");
+                        String imagemref=jsonHITS.getString("webformatURL");
+                        int downloads=jsonHITS.getInt("likes");
+                        itemArrayList.add(new Item(imagemref,criador,downloads));
+
+
+                    }
+                    itemAdapter=new ItemAdapter(MainActivity.this,itemArrayList);
+                    recyclerView.setAdapter(itemAdapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.getStackTrace();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
