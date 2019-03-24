bbb50e59ae24cc1ae23b84e7317146ff326e5e45
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2019-03-24 19:19:23 -0300

    recycler adapter

--- app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/ItemAdapter.java
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/ItemAdapter.java
@@ -1,15 +1,66 @@
 package com.organizze.jmdevelopers.recyclerviewjson;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
-    public class ItemViewHolder extends RecyclerView.ViewHolder{
+    private Context mcontext;
+    private ArrayList<Item> mitems;
 
+    public ItemAdapter(Context context, ArrayList<Item> items) {
+        mcontext = context;
+        mitems = items;
+
+
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view=LayoutInflater.from(mcontext).inflate(R.layout.exemploitem,viewGroup,false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder itemViewHolder, int i) {
+        Item itematual=mitems.get(i);
+        String imagemURL=itematual.getmImagemURL();
+        String nomecriador=itematual.getmCriador();
+        int mdownloads=itematual.getmLikes();
+        itemViewHolder.criador.setText(nomecriador);
+        itemViewHolder.downloads.setText("Likes :"+mdownloads);
+        Picasso.get().load(imagemURL).fit().centerInside().into(itemViewHolder.imagemprincipal);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mitems.size();
+    }
+
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imagemprincipal;
+        public TextView criador;
+        public TextView downloads;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
+            imagemprincipal = itemView.findViewById(R.id.fotoprincipal);
+            criador = itemView.findViewById(R.id.textocriador);
+            downloads = itemView.findViewById(R.id.textodownload);
+
         }
     }
 }
--- app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/MainActivity.java
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/MainActivity.java
@@ -2,12 +2,23 @@ package com.organizze.jmdevelopers.recyclerviewjson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Item> itemArrayList;
+    private ItemAdapter itemAdapter;
+    private RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
