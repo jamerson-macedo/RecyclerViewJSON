4e73dbff646f3aa3175a2f5448ee35167a996ca5
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2019-03-24 18:55:32 -0300

    Initial commit

---
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/Item.java
@@ -0,0 +1,38 @@
+package com.organizze.jmdevelopers.recyclerviewjson;
+
+public class Item {
+    private String mImagemURL;
+    private String mCriador;
+    private int mLikes;
+
+    public String getmImagemURL() {
+        return mImagemURL;
+    }
+
+    public void setmImagemURL(String mImagemURL) {
+        this.mImagemURL = mImagemURL;
+    }
+
+    public String getmCriador() {
+        return mCriador;
+    }
+
+    public void setmCriador(String mCriador) {
+        this.mCriador = mCriador;
+    }
+
+    public int getmLikes() {
+        return mLikes;
+    }
+
+    public void setmLikes(int mLikes) {
+        this.mLikes = mLikes;
+    }
+
+    public Item(String mImagemURL, String mCriador, int mLikes) {
+        this.mImagemURL = mImagemURL;
+        this.mCriador = mCriador;
+        this.mLikes = mLikes;
+    }
+
+}
---
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewjson/ItemAdapter.java
@@ -0,0 +1,15 @@
+package com.organizze.jmdevelopers.recyclerviewjson;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+    public class ItemViewHolder extends RecyclerView.ViewHolder{
+
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
